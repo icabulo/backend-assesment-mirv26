4c07aefd5e015e7b014d29492f1108b1df4b08d4
Ivan <icabulo@gmail.com>
2023-05-01 00:33:05 -0500

    initial users endpoints with login middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
+import {
+  login,
+  generateToken,
+  createUser,
+} from "../controllers/users.controllers.js";
 
 const router = express.Router();
 
 // Login user by email/password
-router.post("/", async (req, res) => {
-  res.send("Login user by email/password");
-});
+router.post("/auth/local/login", login, generateToken);
+
+router.post("/singup", createUser);
 
 export default router;
