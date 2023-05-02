55d74f7c4e980f7d74d7075c67627e97e00a3656
Ivan <icabulo@gmail.com>
2023-05-02 01:24:51 -0500

    feature/validate-user-creation

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,12 +4,23 @@ import {
   generateToken,
   createUser,
 } from "../controllers/users.controllers.js";
+import {
+  validEmail,
+  validPassword,
+  checkErrors,
+} from "../middlewares/validUser.js";
 
 const router = express.Router();
 
 // Login user by email/password
 router.post("/auth/local/login", login, generateToken);
 
-router.post("/singup", createUser);
+// create a new user
+router.post(
+  "/singup",
+  [validEmail(), validPassword()],
+  checkErrors,
+  createUser
+);
 
 export default router;
